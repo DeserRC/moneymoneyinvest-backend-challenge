6f0aef6e1dc7b53df9cb00562b3e6924cc3edb01
DeserRC <deserrc@gmail.com>
2022-07-08 02:38:30 -0300

    Added phone, email and invoicing validation

--- model/entity/client.go
+++ model/entity/client.go
@@ -4,9 +4,9 @@ import "gopkg.in/validator.v2"
 
 type Client struct {
 	Name  string `json:"name" validate:"nonzero"`
-	Cpf   string `json:"cpf" validate:"regexp=^([\\d]{3}\\.[\\d]{3}\\.[\\d]{3}\\-[\\d]{2})|([\\d]{11})$"`
-	Phone string `json:"phone"`
-	Email string `json:"email"`
+	Cpf   string `json:"cpf" validate:"regexp=^([\\d]{3}\\.[\\d]{3}\\.[\\d]{3}\\-[\\d]{2}|[\\d]{11})$"`
+	Phone string `json:"phone" validate:"regexp=^([0]?[\\d]{2})?[\\s]?([\\d]{8}|[\\d]{9})$"`
+	Email string `json:"email" validate:"regexp=^[\\w\\d]+@[\\w\\d]+\\.[\\w\\d]+$"`
 }
 
 func ValidateClient(client *Client) error {
--- model/entity/enterprise.go
+++ model/entity/enterprise.go
@@ -6,7 +6,7 @@ import (
 
 type Enterprise struct {
 	Cnpj      string `json:"cnpj" validate:"regexp=^([\\d]{2}\\.[\\d]{3}\\.[\\d]{3}\\/[\\d]{4}\\-[\\d]{2})|([\\d]{14})$"`
-	Invoicing int    `json:"invoicing"`
+	Invoicing int    `json:"invoicing" validate:"min=0"`
 	Address   string `json:"address" validate:"nonzero"`
 	Requester Client `json:"requester" validate:"nonnil"`
 }
